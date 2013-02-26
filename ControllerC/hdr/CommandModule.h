59f2c342c16ac40cee7f6b5e0dc12d50ec8dabcc
Facundo Martinez Correa <fnmartinez88@gmail.com>
2013-02-25 21:24:01 -0300

    CommandModule beginning.

--- ControllerC/hdr/CommandModule.h
+++ ControllerC/hdr/CommandModule.h
@@ -3,7 +3,32 @@
 
 #include <Ethernet.h>
 
+#define OK_RESPONSE "+OK "
+#define ERROR_RESPONSE "-ERR "
+#define RESPONSE_TERMINATOR "\r\n.\r\n"
+#define MAX_COMMAND_SIZE 257
+#define MAX_RESPONSE_SIZE 257
+#define COMMAND_MAX_SIZE 16
+#define TOTAL_COMMANDS 11
+
+enum Commands_enum {
+	NOOP,
+	TURN_ON,
+	TURN_OFF,
+	SHOW_NETWORK_INFO,
+	SET_SERVER_IP,
+	SET_SERVER_PORT,
+	SENSOR_ON,
+	SENSOR_OFF,
+	GET_STATUS,
+	INFORM_STATUS,
+	EXIT
+};
+
+typedef enum Commands_enum Commands_t;
+
 void beginTCP(int port);
 void verifyIncomingCommand();
+void respond(EthernetClient c, char * response, char * msg, char * terminator);
 
 #endif /* COMMAND_MODULE_H_ */
--- ControllerC/src/commandModule.cpp
+++ ControllerC/src/commandModule.cpp
@@ -1,60 +1,113 @@
 #include <CommandModule.h>
 
+Commands_t commands[] = {
+	NOOP,
+	TURN_ON,
+	TURN_OFF,
+	SHOW_NETWORK_INFO,
+	SET_SERVER_IP,
+	SET_SERVER_PORT,
+	SENSOR_ON,
+	SENSOR_OFF,
+	GET_STATUS,
+	INFORM_STATUS,
+	EXIT
+};
+
+char * commandsString[] = {
+	"NOOP",
+	"TURN_ON",
+	"TURN_OFF",
+	"SET_NETWORK_INFO",
+	"SET_SERVER_IP",
+	"SET_SERVER_PORT",
+	"SENSOR_ON",
+	"SENSOR_OFF",
+	"GET_STATUS",
+	"INFORM_STATUS",
+	"EXIT"
+};
+
 EthernetServer * server;
 
 void beginTCP(int port) {
 	server = new EthernetServer(port);
 }
 
+void respond(EthernetClient c, char * response, char * msg, char * terminator) {
+	c.print(response);
+	if(msg != NULL) {
+		c.print(msg);
+	}
+	c.print(terminator);
+}
+
 void verifyIncomingCommand() {
+	bool firstContact = true;
+	char * response;
+	char message[MAX_RESPONSE_SIZE];
+	Commands_t currentState = NOOP;
+	char incommingCmd[MAX_COMMAND_SIZE];
+
 	EthernetClient client = server->available();
-	if (client) {
-	    Serial.println("new client");
-//	    // an http request ends with a blank line
-//	    while (client.connected()) {
-//	      if (client.available()) {
-//	        char c = client.read();
-//	        Serial.write(c);
-//	        // if you've gotten to the end of the line (received a newline
-//	        // character) and the line is blank, the http request has ended,
-//	        // so you can send a reply
-//	        if (c == '\n' && currentLineIsBlank) {
-//	          // send a standard http response header
-//	          client.println("HTTP/1.1 200 OK");
-//	          client.println("Content-Type: text/html");
-//	          client.println("Connnection: close");
-//	          client.println();
-//	          client.println("<!DOCTYPE HTML>");
-//	          client.println("<html>");
-//	                    // add a meta refresh tag, so the browser pulls again every 5 seconds:
-//	          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
-//	          // output the value of each analog input pin
-//	          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-//	            int sensorReading = analogRead(analogChannel);
-//	            client.print("analog input ");
-//	            client.print(analogChannel);
-//	            client.print(" is ");
-//	            client.print(sensorReading);
-//	            client.println("<br />");
-//	          }
-//	          client.println("</html>");
-//	          break;
-//	        }
-//	        if (c == '\n') {
-//	          // you're starting a new line
-//	          currentLineIsBlank = true;
-//	        }
-//	        else if (c != '\r') {
-//	          // you've gotten a character on the current line
-//	          currentLineIsBlank = false;
-//	        }
-//	      }
-	    // give the web browser time to receive the data
-	    delay(1);
-	    // close the connection:
-	    client.stop();
-	    }
-//	    Serial.println("client disonnected");
+
+	while(client.connected()) {
+		if(firstContact) {
+			respond(client, OK_RESPONSE, "HI!", RESPONSE_TERMINATOR);
+			firstContact = false;
+		}
+		if(client.available() > 0) {
+			int i = 0;
+			bool terminatorFound = false;
+			do {
+				incommingCmd[i] = client.read();
+				if(incommingCmd[i] == '\n') {
+					terminatorFound = true;
+					incommingCmd[i] = '\0';
+				}
+			} while (++i < MAX_COMMAND_SIZE && !terminatorFound);
+
+			for(int i = TOTAL_COMMANDS -1;	i>0; i--) {
+				if(strncasecmp(commandsString[i], incommingCmd, strlen(commandsString[i])) == 0){
+					currentState = commands[i];
+				}
+			}
+
+			switch(currentState) {
+			case NOOP:
+				respond(client, OK_RESPONSE, "HI!", RESPONSE_TERMINATOR);
+				break;
+			case TURN_ON:
+				break;
+			case TURN_OFF:
+				break;
+			case SHOW_NETWORK_INFO:
+				response = OK_RESPONSE;
+
+				break;
+			case SET_SERVER_IP:
+				break;
+			case SET_SERVER_PORT:
+				break;
+			case SENSOR_ON:
+				break;
+			case SENSOR_OFF:
+				break;
+			case GET_STATUS:
+				break;
+			case INFORM_STATUS:
+				break;
+			case EXIT:
+				response = OK_RESPONSE;
+				strcpy(message, "BYE!");
+				respond(client, response, message, RESPONSE_TERMINATOR);
+				client.stop();
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
 
 
